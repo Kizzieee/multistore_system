b80a30e2d92f7a39cc014b0b07a7efbee19ce4d4
Kizzelyn <kizseaf@gmail.com>
2025-02-02 03:11:31 +0800

    Add navigation bar to Home component

--- src/Main/Home.js
+++ src/Main/Home.js
@@ -1,7 +1,22 @@
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <nav className="navbar navbar-expand-fluid bg-main">
+        <div className="row container-fluid mx-5 my-2">
+        <div className="col-4 text-color-main h4">
+         <strong> Multistore Logo
+          </strong>
+        </div>
+
+        <div className="col-4">
+        <i class="bi bi-geo-alt"></i>  Allen, Northern Samar
+        </div>
+
+        <div className="col-4">
+          Account and Cart
+        </div>
+        </div>
+      </nav>
     </div>
   );
 }
