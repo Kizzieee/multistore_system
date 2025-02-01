6a6f88c06cdb711781e026f31ce6b865c3528cf6
Kizzelyn <kizseaf@gmail.com>
2025-02-02 03:12:08 +0800

    Update App component to include Account_View and adjust component rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Account_Registration from "../src/Account/Account_Registration";
 import Account_Login from "../src/Account/Account_Login";
-// import Account_View from "../src/Account/Account_View";
+import Account_View from "../src/Account/Account_View";
 import Home from "../src/Main/Home";
+import './Style.css';
+
 
 function App() {
   return (
     <div>
       {/* <Account_Registration /> */}
       
-      <Account_Login />
+      {/* <Account_Login /> */}
       {/* <Account_View /> */}
       <Home />
     </div>
