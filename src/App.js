5646306bde6f136ac55234b424c4eb364a2be93c
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-22 06:41:26 +0800

    Refactor App component: update imports and adjust component rendering for better organization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import Account_Registration from "../src/Account/Account_Registration";
 import Account_Login from "../src/Account/Account_Login";
 import Account_View from "../src/Account/Account_View";
 import Home from "../src/Main/Home";
-import Restaurant from "./Main/Restaurant";
 import Checkout from "./Main/Checkout";
+import Restaurant from "./Main/Restaurant";
+import Nagivation from "./Navigation";
+import Orders from "./Store/Orders";
 import OwnResto from "./Store/OwnResto";
 import Store_Creation from "./Store/Store_Creation";
-import Orders from "./Store/Orders";
-import Nagivation from "./Navigation";
 import "./style.css";
 
 function App() {
   return (
     <div>
-      {/* <Account_Registration /> */}
-      {/* <Account_Login /> */}
+      <Account_Login />
       {/* <Account_View /> */}
       {/* <Home /> */}
       {/* <Restaurant /> */}
-      <Nagivation />
+      {/* <Nagivation /> */}
       {/* <Checkout /> */}
       {/* <OwnResto /> */}
       {/* <Orders /> */}
