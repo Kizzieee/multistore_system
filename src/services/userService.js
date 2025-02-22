67154e16797b052beb03ca31c835e4b6ed9b9404
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-22 20:33:36 +0800

    Refactor authService and userService: enhance error handling by ensuring consistent error message propagation

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,6 +6,27 @@ export const me = async () => {
     return response.data;
   } catch (error) {
     console.error("Failed to get user info: ", error);
-    throw error.response?.data || error;
+    throw error?.response?.data || error?.message || error;
   }
 };
+
+export async function activate(searchParams) {
+  try {
+    const uuid = searchParams.get("uuid");
+    const token = searchParams.get("token");
+
+    if (!uuid || !token) {
+      throw new Error("Invalid activation link.");
+    }
+
+    const response = await api.post("auth/users/activation/", {
+      uid: uuid,
+      token: token,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Failed to activate account: ", error);
+    throw error?.response?.data || error?.message || error;
+  }
+}
