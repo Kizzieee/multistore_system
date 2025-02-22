d21202250196e0963a1fa7cab63e96403ce8deed
Kizzelyn <kizseaf@gmail.com>
2025-02-22 09:14:34 +0800

    Fix import path for Checkout component and add Activation component import in App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Account_Login from "../src/Account/Account_Login";
 import Account_View from "../src/Account/Account_View";
 import Home from "../src/Main/Home";
 import Restaurant from "./Main/Restaurant";
-import Checkout from "./Main/Checkout";
+import Checkout from "./Store/Checkout";
 import OwnResto from "./Store/OwnResto";
 import Store_Creation from "./Store/Store_Creation";
 import Orders from "./Store/Orders";
 import Nagivation from "./Navigation";
+import Activation from "./Account/AccountActivation";
 import "./style.css";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       {/* <Checkout /> */}
       {/* <OwnResto /> */}
       {/* <Orders /> */}
+      {/* <Activation /> */}
     </div>
   );
 }
