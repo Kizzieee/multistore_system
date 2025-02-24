7a1bfbe2fb7864e7cea8ec3588b795d433fdcd87
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-25 00:36:11 +0800

    Enhance ProtectedRoute to redirect non-logged-in users and store owners

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,11 +1,25 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const ProtectedRoute = ({ isLoggedIn, setIsModalOpen, children }) => {
+const ProtectedRoute = ({
+  isLoggedIn,
+  setIsModalOpen,
+  isStoreOwner,
+  children,
+}) => {
   const navigate = useNavigate();
-  if (!isLoggedIn) {
-    setIsModalOpen(true);
-    return navigate("/");
-  }
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      setIsModalOpen(true);
+      navigate("/");
+    }
+    if (location.pathname === "/own-resto" && !isStoreOwner) {
+      navigate("/account");
+    }
+  }, [isLoggedIn, isStoreOwner, navigate, setIsModalOpen, location.pathname]);
+
   return children;
 };
 
