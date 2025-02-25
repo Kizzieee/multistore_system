dce18669f5c01398feaaa4b83ca5f3ae78ffe427
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-25 20:30:55 +0800

    Add updateStore and fetchMyStore functions to manage store data

--- src/services/storeService.js
+++ src/services/storeService.js
@@ -31,3 +31,47 @@ export const createStore = async (data) => {
     throw error?.response?.data || error?.message || error;
   }
 };
+
+export const updateStore = async (storeId, data) => {
+  try {
+    const formData = new FormData();
+
+    if (data.name) formData.append("name", data.name);
+    if (data.email) formData.append("email", data.email);
+    if (data.mobile_number)
+      formData.append("mobile_number", data.mobile_number);
+    if (data.delivery_fee) formData.append("delivery_fee", data.delivery_fee);
+    if (data.description) formData.append("description", data.description);
+    if (data.opening_time) formData.append("opening_time", data.opening_time);
+    if (data.closing_time) formData.append("closing_time", data.closing_time);
+
+    if (data.address?.city) formData.append("address.city", data.address.city);
+    if (data.address?.province)
+      formData.append("address.province", data.address.province);
+
+    if (data.image) {
+      formData.append("image", data.image);
+    }
+
+    const response = await api.patch(`store/stores/${storeId}/`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Failed to update store: ", error);
+    throw error?.response?.data || error?.message || error;
+  }
+};
+
+export const fetchMyStore = async () => {
+  try {
+    const response = await api.get(`store/stores/my_store/`);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch your store: ", error);
+    throw error?.response?.data || error?.message || error;
+  }
+};
