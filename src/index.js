b30bfc601da109709343c72786d428015eed85b6
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-27 00:24:23 +0800

    Wrap App component with GlobalProvider for context management

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
+import { GlobalProvider } from "./GlobalContext";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </HashRouter>
 );
 
