04ea656a9f98fb4c3c6f88fcd367f81b92fafea5
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-27 00:25:06 +0800

    Refactor ProtectedRoute to use GlobalContext for user state and add loading state

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,24 +1,50 @@
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { GlobalContext } from "./GlobalContext";
 
-const ProtectedRoute = ({
-  isLoggedIn,
-  setIsModalOpen,
-  isStoreOwner,
-  children,
-}) => {
+const ProtectedRoute = ({ setIsModalOpen, children }) => {
+  const { user, isLoggedIn, isStoreOwner } = useContext(GlobalContext);
   const navigate = useNavigate();
   const location = useLocation();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (user === null && isLoggedIn === null && isStoreOwner === null) {
+      return;
+    }
+
+    setLoading(false);
+
     if (!isLoggedIn) {
+      navigate("/");
       setIsModalOpen(true);
+    }
+
+    if (location.pathname === "/own-resto" && !isStoreOwner) {
       navigate("/");
     }
-    if (location.pathname === "/own-resto" && !isStoreOwner) {
-      navigate("/account");
-    }
-  }, [isLoggedIn, isStoreOwner, navigate, setIsModalOpen, location.pathname]);
+  }, [
+    user,
+    isLoggedIn,
+    isStoreOwner,
+    navigate,
+    setIsModalOpen,
+    location.pathname,
+  ]);
+
+  if (loading) {
+    console.log("user", user);
+    console.log("isLoggedIn", isLoggedIn);
+    console.log("isStoreOwner", isStoreOwner);
+
+    return (
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
 
   return children;
 };
