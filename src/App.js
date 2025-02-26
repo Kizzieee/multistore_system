b0843df409805a39c430e5c759fe8eaa3ac417c1
Joveth-Dev <jovethespiritu.developer@gmail.com>
2025-02-26 21:19:38 +0800

    Remove unused component imports from App.js for cleaner code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import AccountLogin from "../src/Account/AccountLogin";
-import AccountView from "../src/Account/AccountView";
-import Home from "../src/Main/Home";
-import Activation from "./Account/AccountActivation";
-// import Checkout from "./Main/Checkout";
-import Restaurant from "./Main/Restaurant";
 import Nagivation from "./Navigation";
-import Orders from "./Store/Orders";
-import OwnResto from "./Store/OwnResto";
-import StoreCreation from "./Store/StoreCreation";
 import "./style.css";
 
 function App() {
   return (
     <div>
-      {/* <AccountLogin /> */}
-      {/* <AccountView /> */}
-      {/* <Home /> */}
-      {/* <Restaurant /> */}
       <Nagivation />
-      {/* <Checkout /> */}
-      {/* <OwnResto /> */}
-      {/* <Orders /> */}
-      {/* <Activation /> */}
     </div>
   );
 }
